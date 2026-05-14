dfec3ce3aeadfe1f3731635bc778c3ff98d91eb9
antonio baccaro <antoniobaccaro@MacBook-Pro-di-antonio.local>
2026-05-14 11:49:33 +0200

    Add CV Barrano Rocky (fisicaro-08) and Di Pietro Giorgia (fisicaro-15)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-const DATA_URL = "assets/data/site-data.json?v=20260513-fdi";
+const DATA_URL = "assets/data/site-data.json?v=20260513-fisicaro";
 
 const escapeHtml = (value = "") =>
   String(value)
