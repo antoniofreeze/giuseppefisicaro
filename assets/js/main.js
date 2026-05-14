9afd28cc4e7ee827bb98cc92bf4fc773020dd444
antonio baccaro <antoniobaccaro@MacBook-Pro-di-antonio.local>
2026-05-14 11:46:51 +0200

    Add CV Mattia Oliva (fisicaro-10)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-const DATA_URL = "assets/data/site-data.json?v=20260513-giovane";
+const DATA_URL = "assets/data/site-data.json?v=20260513-oliva";
 
 const escapeHtml = (value = "") =>
   String(value)
