da7b35e2d9c59e3f6aeb0449e3c4719400eda54d
antonio baccaro <antoniobaccaro@MacBook-Pro-di-antonio.local>
2026-05-14 11:41:32 +0200

    Lentini Giovane: add CVs and casellari; add Fava casellario

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-const DATA_URL = "assets/data/site-data.json?v=20260511-docs";
+const DATA_URL = "assets/data/site-data.json?v=20260513-giovane";
 
 const escapeHtml = (value = "") =>
   String(value)
